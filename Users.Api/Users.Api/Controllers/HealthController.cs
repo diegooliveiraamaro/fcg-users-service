3d9dfed2fb7bf9703d391c40896366fd45a28384
Diego dev <diego.oliveiraamaro@gmail.com>
2026-01-06 18:22:54 -0300

    teste 9

---
+++ Users.Api/Users.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Users.Api.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
