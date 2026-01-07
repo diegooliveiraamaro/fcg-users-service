2d1c802878aa1d8bf0ae0044a9149a560cb4e825
Diego dev <diego.oliveiraamaro@gmail.com>
2026-01-07 00:18:29 -0300

    teste 12

--- Users.Api/Users.Api/Program.cs
+++ Users.Api/Users.Api/Program.cs
@@ -25,10 +25,45 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// ✅ Health endpoint para ALB
+// ✅ ENDPOINT ÚNICO PARA O ALB
 app.MapGet("/health", () => Results.Ok("Healthy"));
 
-// ESSENCIAL para Docker / ECS
 app.Urls.Add("http://0.0.0.0:80");
 
 app.Run();
+
+
+//using Microsoft.EntityFrameworkCore;
+//using Users.Api.Infrastructure.Data;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<UsersDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+//);
+
+//var app = builder.Build();
+
+//app.UseSwagger();
+//app.UseSwaggerUI(c =>
+//{
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
+//    c.RoutePrefix = "swagger";
+//});
+
+//app.UseRouting();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//// ✅ Health endpoint para ALB
+//app.MapGet("/health", () => Results.Ok("Healthy"));
+
+//// ESSENCIAL para Docker / ECS
+//app.Urls.Add("http://0.0.0.0:80");
+
+//app.Run();
