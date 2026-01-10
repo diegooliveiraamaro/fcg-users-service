12a9ab70f02637de0030383743b497ca25ba8e99
Diego dev <diego.oliveiraamaro@gmail.com>
2026-01-10 13:21:22 -0300

    teste 5

--- Users.Api/Users.Api/Program.cs
+++ Users.Api/Users.Api/Program.cs
@@ -32,8 +32,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    //c.SwaggerEndpoint("/users/swagger/v1/swagger.json", "Users API v1");
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
+    c.SwaggerEndpoint("/users/swagger/v1/swagger.json", "Users API v1");
+    //c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
     c.RoutePrefix = "swagger";
 });
 //}
