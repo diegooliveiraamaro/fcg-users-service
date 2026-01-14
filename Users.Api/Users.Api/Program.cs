56e448d0146436a84e89652b8da8a617b3b2c3de
Diego dev <diego.oliveiraamaro@gmail.com>
2026-01-13 22:06:32 -0300

    Git Action User.Api

--- Users.Api/Users.Api/Program.cs
+++ Users.Api/Users.Api/Program.cs
@@ -18,37 +18,31 @@ builder.Services.AddDbContext<UsersDbContext>(options =>
 var app = builder.Build();
 
 
-//if (Debugger.IsAttached)
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(c =>
-//    {
-//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
-//        c.RoutePrefix = "swagger";
-//    });
-//}
-//else
-//{
-//app.UseSwagger();
-//app.UseSwaggerUI(c =>
-//{
-//    c.SwaggerEndpoint("/users/swagger/v1/swagger.json", "Users API v1");
-//    //c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
-//    c.RoutePrefix = "swagger";
-//});
-//}
-
-
-app.UseSwagger(c =>
+if (Debugger.IsAttached)
 {
-    c.RouteTemplate = "users/swagger/{documentName}/swagger.json";
-});
 
-app.UseSwaggerUI(c =>
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
+        c.RoutePrefix = "swagger";
+    });
+}
+else
 {
-    c.SwaggerEndpoint("/users/swagger/v1/swagger.json", "Users API v1");
-    c.RoutePrefix = "users/swagger";
-});
+
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "users/swagger/{documentName}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/users/swagger/v1/swagger.json", "Users API v1");
+        c.RoutePrefix = "users/swagger";
+    });
+}
+
 
 app.UseRouting();
 app.UseAuthorization();
@@ -61,39 +55,3 @@ app.MapGet("/health", () => Results.Ok("Healthy"));
 app.Urls.Add("http://0.0.0.0:80");
 
 app.Run();
-
-
-//using Microsoft.EntityFrameworkCore;
-//using Users.Api.Infrastructure.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//builder.Services.AddDbContext<UsersDbContext>(options =>
-//    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
-//);
-
-//var app = builder.Build();
-
-//app.UseSwagger();
-//app.UseSwaggerUI(c =>
-//{
-//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API v1");
-//    c.RoutePrefix = "swagger";
-//});
-
-//app.UseRouting();
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//// ✅ Health endpoint para ALB
-//app.MapGet("/health", () => Results.Ok("Healthy"));
-
-//// ESSENCIAL para Docker / ECS
-//app.Urls.Add("http://0.0.0.0:80");
-
-//app.Run();
